95c96223074b1c604a084f4d09d14eb2d5aafda9
Stuart <eagle1@live.co.uk>
2018-11-30 09:35:02 +0000

    Corrected Spelling Mistake

--- Cacluator1/Cacluator1/Form1.Designer.cs
+++ Cacluator1/Cacluator1/Form1.Designer.cs
@@ -28,13 +28,45 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnAnswer = new System.Windows.Forms.Button();
+            this.tbFirstNumber = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnAnswer
+            // 
+            this.btnAnswer.Location = new System.Drawing.Point(174, 107);
+            this.btnAnswer.Name = "btnAnswer";
+            this.btnAnswer.Size = new System.Drawing.Size(75, 23);
+            this.btnAnswer.TabIndex = 0;
+            this.btnAnswer.Text = "button1";
+            this.btnAnswer.UseVisualStyleBackColor = true;
+            // 
+            // tbFirstNumber
+            // 
+            this.tbFirstNumber.Location = new System.Drawing.Point(97, 34);
+            this.tbFirstNumber.Name = "tbFirstNumber";
+            this.tbFirstNumber.Size = new System.Drawing.Size(100, 20);
+            this.tbFirstNumber.TabIndex = 1;
+            this.tbFirstNumber.Text = "10";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tbFirstNumber);
+            this.Controls.Add(this.btnAnswer);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnAnswer;
+        private System.Windows.Forms.TextBox tbFirstNumber;
     }
 }
 
